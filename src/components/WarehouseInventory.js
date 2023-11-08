171a398e0cc0d6c26796db7edc6f1ae0e7be8fdd
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-08 13:30:20 -0500

    beginning styling with mock data

--- src/components/WarehouseInventory.js
+++ src/components/WarehouseInventory.js
@@ -11,68 +11,81 @@ function WarehouseInventory({warehouseId}) {
     const [inventoryItems, setInventoryItems] = useState([]);
 
     // Get inventory items for specific specific warehouse 
-    useEffect(() => {
-        const fetchInventory = async () => {
-        try {
-            const response = await axios.get(`http://localhost:8080/warehouse/${warehouseId}/inventory`);
-            setInventoryItems(response.data);
-        }   catch (error) {
-                console.error('Error fetching inventory data:', error);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchInventory = async () => {
+    //     try {
+    //         const response = await axios.get(`http://localhost:8080/warehouse/${warehouseId}/inventory`);
+    //         setInventoryItems(response.data);
+    //     }   catch (error) {
+    //             console.error('Error fetching inventory data:', error);
+    //         }
+    // };
 
-    fetchInventory();
-    }, [warehouseId]);
+    // fetchInventory();
+    // }, [warehouseId]);
 
-    // Delete specific inventory item for specific warehouse 
-    const handleDelete = async (itemId) => {
-        try {
-            await axios.delete(`http://localhost:8080/warehouse/${warehouseId}/inventory/${itemId}`);
-            setInventoryItems(inventoryItems.filter(item => item.id !== itemId));
-        } catch (error) {
-        console.error('Error deleting inventory item:', error);
-        }
-    };
+    // // Delete specific inventory item for specific warehouse 
+    // const handleDelete = async (itemId) => {
+    //     try {
+    //         await axios.delete(`http://localhost:8080/warehouse/${warehouseId}/inventory/${itemId}`);
+    //         setInventoryItems(inventoryItems.filter(item => item.id !== itemId));
+    //     }   catch (error) {
+    //             console.error('Error deleting inventory item:', error);
+    //         }
+    // };
 
-    const handleEdit = (itemId) => {
-        // Edit item 
-        // Navigate to edit page (route has not been created yet)
-    };
+    // const handleEdit = (itemId) => {
+    //     // Edit item 
+    //     // Navigate to edit page (route has not been created yet)
+    // };
 
     return (
         <section className="warehouse-inventory">
-            {inventoryItems.map((item) => (
-            <article key={item.id} className="warehouse-inventory__item">
+            <article className="warehouse-inventory__item">
                 <div>
-                    <h2>Inventory Item{item.item_name}</h2>
-                    <img
-                        className='warehouse-inventory__actions-details'
-                        src={rightArrowIcon}
-                        alt="Details"
-                    />
-                    {/* <p>{item.description}</p> */}
-                    <div className="warehouse-inventory__details">
-                        <span className="warehouse-inventory__category">Category{item.category}</span>
-                        <span className="warehouse-inventory__status">Status{item.status}</span>
-                        <span className="warehouse-inventory__quantity">Qty{item.quantity}</span>
-                    </div>
-                    <div className="warehouse-inventory__actions">
+                    <h2>Inventory Item</h2>
+                    <div>
+                        <p>Television</p>
                         <img
-                            className='warehouse-inventory__actions-delete'
-                            src={deleteIcon}
-                            alt="Delete"
-                            onClick={() => handleDelete(item.id)}
-                        />
-                        <img
-                            className='warehouse-inventory__actions-edit'
-                            src={editIcon}
-                            alt="Edit"
-                            onClick={() => handleEdit(item.id)}
+                            className='warehouse-inventory__actions-details'
+                            src={rightArrowIcon}
+                            alt="Details"
                         />
                     </div>
                 </div>
+                <div className="warehouse-inventory__details">
+                    <div>
+                        <h2>Category</h2>
+                        <p className="warehouse-inventory__category">Electronics</p>
+                    </div>
+                    <div>
+                        <h2>Status</h2>
+                        <p className="warehouse-inventory__status">In Stock</p>
+                    </div>
+                    <div>
+                        <h2>Qty</h2>
+                        <p className="warehouse-inventory__quantity">500</p>
+                    </div>
+                    <div>
+                        <h2>Warehouse</h2>
+                        <p>Manhattan</p>
+                    </div>
+                </div>
+                <div className="warehouse-inventory__actions">
+                    <img
+                        className='warehouse-inventory__actions-delete'
+                        src={deleteIcon}
+                        alt="Delete"
+                        // onClick={() => handleDelete(item.id)}
+                    />
+                    <img
+                        className='warehouse-inventory__actions-edit'
+                        src={editIcon}
+                        alt="Edit"
+                        // onClick={() => handleEdit(item.id)}
+                    />
+                </div>
             </article>
-        ))}
         </section>
     );
 }
@@ -81,5 +94,4 @@ export default WarehouseInventory;
 
 // Review Questions: 
 // Should i replace 8080 with 'PORT'?
-// Abbreviation for warehouse / inventory? 
 // Double check endpoints (warehouse/warehouse-id/inventory/inventory-id
