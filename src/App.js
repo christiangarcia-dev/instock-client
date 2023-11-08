4b9e65a6cf63d917135b8fc4afe5578995aff587
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-08 16:32:49 -0500

    corrected names and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WarehouseDetails from './components/Warehouse-Details/Warehouse-Details';
+import WarehouseList from './components/Warehouse-Details/Warehouse-List';
 
 function App() {
   return (
     <div className="App">
-    
+  
       <BrowserRouter>
         <Routes>
-         <Route path='/warehouse-details' element={<WarehouseDetails/>}/>
+         <Route path='/warehouse-list' element={<WarehouseList/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Warehouse-Details/Warehouse-List.js
@@ -0,0 +1,43 @@
+import './Warehouse-List.scss';
+import editIcon from '../../assets/icons/edit-24px.svg';
+import deleteIcon from '../../assets/icons/delete_outline-24px.svg'
+// import { Link } from 'react-router-dom';
+import chevronRight from '../../assets/icons/chevron_right-24px.svg';
+
+function WarehouseList() {
+    return (
+    <section className='warehouse__list' >
+         <section className='warehouselist__containerOne'>
+            <h2 className='warehouselist__containerOne-title'>Warehouses</h2>
+            <input className='warehouselist__containerOne-search' type='text' placeholder='Search...'></input>
+            <button className='warehouselist__containerOne-add'>+ Add New Warehouse</button>
+         </section>
+        
+         <section className='warehouselist__containerTwo'>
+            <div className='warehouse__allWarehouses'>
+                <div className='warehouse__allWarehouses-one'>
+                    <h6 className='warehouse__allWarehouses-titleWare'>WAREHOUSE</h6>
+                    <p className='warehouse__allWarehouses-typeItem'>Item <img src={chevronRight}/> </p>
+                    <h6 className='warehouse__allWarehouses-titleAddress'>ADDRESS</h6>
+                    <p className='warehouse__allWarehouses-category'>CategoryType</p>
+                </div>
+                <div className='warehouse__allWarehouses-two'>
+                    <h6 className='warehouse__allWarehouses-titleContact'>CONTACT NAME</h6>
+                    <p className='warehouse__allWarehouses-stock'>In stock</p>
+                    <h6 className='warehouse__allWarehouses-titleInfo'>CONTACT INFORMATION</h6>
+                    <p  className='warehouse__allWarehouses-quantity'>Quantity</p>
+                </div>
+            </div>
+            <div className='warehouse__allWarehouses-buttons'>
+                <img className='warehouse__allWarehouses-delete' src={deleteIcon}/>
+                <img className='warehouse__allWarehouses-edit' src={editIcon}/>
+            </div>
+         </section>
+    </section>
+
+    // I HAVE TO ADD ALT FOR THR ICONS-IMAGES
+    )
+
+}
+
+export default WarehouseList;
