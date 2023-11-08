abed3fcbf9f8486f127f8c8089830b6d70dca0cc
Paulette Blanc <pauletteblanc@Paulettes-Air.localdomain>
2023-11-08 08:48:12 -0500

    initial form structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.scss';
+import EditWarehouse from './components/EditWarehouse/EditWarehouse';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <EditWarehouse/>
 
     </div>
   );
