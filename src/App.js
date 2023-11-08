92f3062ddbaf50306f631a1756bcc80d5c4d7b4b
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-08 14:32:39 -0500

    inital styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.scss';
 import WarehouseInventoryPage from './pages/WarehouseInventoryPage/WarehouseInventoryPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WarehouseDetails from './components/Warehouse-Details/Warehouse-Details';
+// import WarehouseDetails from './components/Warehouse-Details/Warehouse-Details';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <WarehouseInventoryPage />
       <BrowserRouter>
         <Routes>
-          <Route path='/warehouse-details' element={<WarehouseDetails/>}/>
+          {/* <Route path='/warehouse-details' element={<WarehouseDetails/>}/> */}
         </Routes>
       </BrowserRouter>
     </div>
