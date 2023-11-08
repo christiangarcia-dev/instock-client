bb3b8f74a2ef6ccc8518f6eb505b1592bc2009e8
Raphael Da Rocha <raphaeldr@RaphMac.localdomain>
2023-11-08 16:21:35 -0500

    added extra padding

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WarehouseDetails from './components/Warehouse-Details/Warehouse-Details';
 
+
 function App() {
   return (
     <div className="App">
