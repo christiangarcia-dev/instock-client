c01debafe3f7beb7bbe58ee54f003b02930980d3
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-08 17:08:17 -0500

    added style for tablet

--- src/components/Warehouse-Details/Warehouse-List.js
+++ src/components/Warehouse-Details/Warehouse-List.js
@@ -3,21 +3,27 @@ import editIcon from '../../assets/icons/edit-24px.svg';
 import deleteIcon from '../../assets/icons/delete_outline-24px.svg'
 // import { Link } from 'react-router-dom';
 import chevronRight from '../../assets/icons/chevron_right-24px.svg';
+import { NavLink } from 'react-router-dom';
 
 function WarehouseList() {
+    // const hanldeAddNew = () => {
+    //    return <NavLink to={<Warehouse-Details>}>
+    // }
     return (
     <section className='warehouse__list' >
          <section className='warehouselist__containerOne'>
             <h2 className='warehouselist__containerOne-title'>Warehouses</h2>
-            <input className='warehouselist__containerOne-search' type='text' placeholder='Search...'></input>
-            <button className='warehouselist__containerOne-add'>+ Add New Warehouse</button>
+            <div>
+                <input className='warehouselist__containerOne-search' type='text' placeholder='Search...'></input>
+                <button className='warehouselist__containerOne-add'>+ Add New Warehouse</button>
+            </div>
          </section>
         
          <section className='warehouselist__containerTwo'>
             <div className='warehouse__allWarehouses'>
                 <div className='warehouse__allWarehouses-one'>
                     <h6 className='warehouse__allWarehouses-titleWare'>WAREHOUSE</h6>
-                    <p className='warehouse__allWarehouses-typeItem'>Item <img src={chevronRight}/> </p>
+                        <p className='warehouse__allWarehouses-typeItem'>Item <img src={chevronRight}/> </p>
                     <h6 className='warehouse__allWarehouses-titleAddress'>ADDRESS</h6>
                     <p className='warehouse__allWarehouses-category'>CategoryType</p>
                 </div>
