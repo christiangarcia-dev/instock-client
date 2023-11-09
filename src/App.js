c2ccb4758e586738565e4892dd09ae11c33c1c9a
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-09 12:34:52 -0500

    change name file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WarehouseList from './components/Warehouse-Details/Warehouse-List';
+import WarehouseList from './components/Warehouse-List/Warehouse-List';
 import Header from "./components/Header/Header";
 
 
