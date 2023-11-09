7e2217d6812f8f93b6597da53edcab9ebca10b50
Raphael Da Rocha <raphaeldr@RaphMac.localdomain>
2023-11-09 13:08:56 -0500

    added warehouse details structure

--- src/components/Warehouse-Details/Warehouse-Details.js
+++ src/components/Warehouse-Details/Warehouse-Details.js
@@ -27,26 +27,7 @@ function WarehouseDetails() {
                 </div>
             </div>
          </section>
-         <section className='warehousedetails__containerThree'>
-            <div className='warehouse__inventory'>
-                <div className='warehouse__inventory-one'>
-                    <h4 className='warehouse__inventory-titleInve'>INVENTORY ITEM</h4>
-                    <p className='warehouse__inventory-typeItem'>Item</p>
-                    <h4 className='warehouse__inventory-titleCate'>CATEGORY</h4>
-                    <p className='warehouse__inventory-category'>CategoryType</p>
-                </div>
-                <div className='warehouse__inventory-two'>
-                    <h4 className='warehouse__inventory-titleStatus'>STATUS</h4>
-                    <p className='warehouse__inventory-stock?'>In stock</p>
-                    <h4 className='warehouse__inventory-titleQTY'>QTY</h4>
-                    <p  className='warehouse__inventory-quantity'>Quantity</p>
-                </div>
-            </div>
-            <div className='warehouse__inventory-buttons'>
-                <button className='warehouse__inventory-delete'>DELETE</button>
-                <button className='warehouse__inventory-edit'>EDIT</button>
-            </div>
-         </section>
+         
     </section>
     )
 
