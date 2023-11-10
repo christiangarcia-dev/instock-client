e226b7f2a3415bbb0b9eb3448fe069fe89a1f461
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-10 13:59:04 -0500

    solved merge conflict

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
           <Route path='/' element={<HomePage />}/>
           <Route path='/warehouse-details' element={<WarehouseDetailsPage />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </div>
   );
