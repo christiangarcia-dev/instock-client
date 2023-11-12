75ae4b6cf80fd87c88dd08632d52e7aea3353a3f
Raphael Da Rocha <raphaeldr@RaphMac.local>
2023-11-11 20:23:24 -0500

    added InventoryListPage for InventoryList

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage';
 import HomePage from './pages/HomePage/HomePage';
-import InventoryList from './components/Inventory-List/Inventory-List'
+import InventoryListPage from './pages/InventoryListPage/InventoryListPage';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />}/>
           <Route path='/warehouse-details' element={<WarehouseDetailsPage />} />
-          <Route path='/inventory'element={<InventoryList/>} />
+          <Route path='/inventory'element={<InventoryListPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
