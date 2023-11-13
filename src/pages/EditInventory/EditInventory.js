47411a6c0981c0773ac3ac70cb0dc0cdaa8902d4
CryptoTotalWar <pablo@dltpartners.io>
2023-11-13 13:08:32 -0500

    clone headers

---
+++ src/pages/EditInventory/EditInventory.js
@@ -0,0 +1,6 @@
+import "./EditInventory.scss";
+import backArrow from "../../assets/icons/arrow_back-24px.svg";
+import errorImg from "../../assets/icons/error-24px.svg";
+import { useState } from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
