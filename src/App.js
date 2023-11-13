9a387f91676cec076dfb05d3b1a2a1d6aaf99670
Paulette Blanc <pauletteblanc@Paulettes-MacBook-Air.local>
2023-11-12 20:57:15 -0500

    remove component from app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage';
 import HomePage from './pages/HomePage/HomePage';
-import EditWarehouse from './components/EditWarehouse/EditWarehouse';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route path='/warehouse-details' element={<WarehouseDetailsPage />} />
         </Routes>
       </BrowserRouter>
-      <EditWarehouse />
     </div>
   );
 }
