70cd5f6364ac67b21a9dbef68221a857869a3412
christiangarcia-dev <2001christiangarcia@gmail.com>
2023-11-13 16:24:54 -0500

    header and warehouse list styles mostly fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import "./Header.scss"
 import logo from "../../assets/logo/InStock-Logo.svg"
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header className="header">
             <div className="header__logo">
-                <img src={logo} className="header__logo-src" alt="instock logo" />
+                <Link to="/">
+                    <img src={logo} className="header__logo-src" alt="instock logo" />
+                </Link>
             </div>
             <div className="header__buttons">
                 <button className="header__buttons-warehouses header__buttons-all">Warehouses</button>
