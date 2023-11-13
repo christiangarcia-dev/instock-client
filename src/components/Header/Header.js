480c08e654d3c5b0de99ca843d5c4a1f637e380b
Raphael Da Rocha <raphaeldr@RaphMac.localdomain>
2023-11-13 15:01:16 -0500

    added linking to header buttons but i must fix styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import "./Header.scss"
 import logo from "../../assets/logo/InStock-Logo.svg"
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
@@ -8,8 +9,12 @@ function Header() {
                 <img src={logo} className="header__logo-src" alt="instock logo" />
             </div>
             <div className="header__buttons">
+                <Link to={`/`} className="header__buttons-link">
                 <button className="header__buttons-warehouses header__buttons-all">Warehouses</button>
+                </Link>
+                <Link to={`/inventory`} className="header__buttons-link">
                 <button className="header__buttons-inventory header__buttons-all">Inventory</button>
+                </Link>
             </div>
         </header>
     )
