bc5cc13fd15f6af05e5b07ee0098c97dad746fa4
Raphael Da Rocha <raphaeldr@RaphMac.localdomain>
2023-11-13 12:06:19 -0500

    changed class names

--- src/components/Inventory-List/Inventory-List.js
+++ src/components/Inventory-List/Inventory-List.js
@@ -20,53 +20,54 @@ function InventoryList() {
 
   
     return (
-    <section className='warehouse__list' >
-        <section className='warehouselist__containerOne'>
-            <h2 className='warehouselist__containerOne-title'>Inventory</h2>
-            <div className='warehouse__nav'>
-                <input className='warehouselist__containerOne-search' type='text' placeholder='Search...'></input>
-                <button className='warehouselist__containerOne-add'>+ Add New Item</button>
-            </div>
-        </section>
-            <div className='warehouselist__titles'>
-                <h6 className='warehouse__allWarehouses--titleWare'>INVENTORY ITEM</h6>
-                <h6 className='warehouse__allWarehouses--titleAddress'>CATEGORY</h6>
-                <h6 className='warehouse__allWarehouses--titleContact'>STATUS</h6>
-                <h6 className='warehouse__allWarehouses--titleQTY'>QTY</h6>
-                <h6 className='warehouse__allWarehouses--titleInfo'>WAREHOUSE</h6>
-                <h6 className='warehouse__allWarehouse-title'>ACTIONS</h6>
+        <section className='inventory__list' >
+            <section className='inventorylist__containerOne'>
+                <h2 className='inventorylist__containerOne-title'>Inventory</h2>
+                <div className='inventory__nav'>
+                    <input className='inventorylist__containerOne-search' type='text' placeholder='Search...'></input>
+                    <button className='inventorylist__containerOne-add'>+ Add New Item</button>
+                </div>
+            </section>
+            <div className='inventorylist__titles'>
+                <h6 className='inventory__allInventories--titleWare'>INVENTORY ITEM</h6>
+                <h6 className='inventory__allInventories--titleAddress'>CATEGORY</h6>
+                <h6 className='inventory__allInventories--titleContact'>STATUS</h6>
+                <h6 className='inventory__allInventories--titleQTY'>QTY</h6>
+                <h6 className='inventory__allInventories--titleInfo'>INVENTORY</h6>
+                <h6 className='inventory__allInventory-title'>ACTIONS</h6>
             </div>
             {
-                inventories?.map ((inventory) => (
-
-                    <section key={inventory.id} className='warehouselist__containerTwo'>
-                        <div className='warehouse__allWarehouses'>
-                            <div className='warehouse__allWarehouses-one'>
-                                <div>
-                                    <h6 className='warehouse__allWarehouses-titleWare'>INVENTORY ITEM</h6>
-                                    <p className='warehouse__allWarehouses-typeItem'> {inventory.item_name}<img src={chevronRight}/></p>
+                inventories?.map((inventory) => (
+                    <section key={inventory.id} className='inventorylist__containerTwo'>
+                        <div className='inventory__allInventories'>
+                            <div className='inventory__allInventories-one'>
+                                <div className='inventory__'>
+                                    <h6 className='inventory__allInventories-titleWare'>INVENTORY ITEM</h6>
+                                    <p className='inventory__allInventories-typeItem'> {inventory.item_name}<img src={chevronRight} /></p>
                                 </div>
                                 <div>
-                                    <h6 className='warehouse__allWarehouses-titleAddress'>CATEGORY</h6>
-                                    <p className='warehouse__allWarehouses-category'>{inventory.category}</p>
+                                    <h6 className='inventory__allInventories-titleAddress'>CATEGORY</h6>
+                                    <p className='inventory__allInventories-category'>{inventory.category}</p>
                                 </div>
                             </div>
-                            <div className='warehouse__allWarehouses-two'>
-                                <h6 className='warehouse__allWarehouses-titleContact'>STATUS</h6>
-                                <p className='warehouse__allWarehouses-stock'>{inventory.status}</p>
-                                <h6 className='warehouse__allWarehouses-titleInfo'>QTY</h6>
-                                <p  className='warehouse__allWarehousescontact_phone'>{inventory.quantity}</p>
-                                <h6 className='warehouse__allWarehouses-titleAddress'>WAREHOUSE</h6>
-                                <p className='warehouse__allWarehouses-category'>{inventory.warehouse_name}</p>
+                            <div className='inventory__allInventories-two'>
+                                <h6 className='inventory__allInventories-titleContact'>STATUS</h6>
+                                <p className='inventory__allInventories-stock'>{inventory.status}</p>
+                                <h6 className='inventory__allInventories-titleInfo'>QTY</h6>
+                                <p className='inventory__allInventoriescontact_phone'>{inventory.quantity}</p>
+                                <h6 className='inventory__allInventories-titleAddress'>INVENTORY</h6>
+                                <p className='inventory__allInventories-category'>{inventory.warehouse_name}</p>
                             </div>
                         </div>
-                        <div className='warehouse__allWarehouses-buttons'>
-                            <img className='warehouse__allWarehouses-delete' src={deleteIcon}/>
-                            <img className='warehouse__allWarehouses-edit' src={editIcon}/>
+                        <div className='inventory__allInventories-buttons'>
+                            <img className='inventory__allInventories-delete' src={deleteIcon} />
+                            <img className='inventory__allInventories-edit' src={editIcon} />
                         </div>
                     </section>
-            ))}
-    </section>
+                ))
+            }
+        </section>
+        
 
     // I HAVE TO ADD ALT FOR THR ICONS-IMAGES
     )
