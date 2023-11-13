1b9eb086d77b33a8dfe5c156bd359fc63a0cd2ec
Paulette Blanc <pauletteblanc@Paulettes-Air.localdomain>
2023-11-13 16:46:51 -0500

    add api call for item details

--- src/components/InventoryItemDetails/InventoryItemDetails.js
+++ src/components/InventoryItemDetails/InventoryItemDetails.js
@@ -1,13 +1,21 @@
 import "../InventoryItemDetails/InventoryItemDetails.scss";
 import backButton from "../../assets/icons/arrow_back-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
-
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 function InventoryItemDetails() {
-
-
+    const { id } = useParams();
+    const [itemData, setItemData] = useState(null);
+    
+    useEffect( () => {
+        async function getInventoryItem(){
+            const response = await axios.get(`http://localhost:8080/api/inventories/${id}`);
+            setItemData(response.data);
+        }
+        getInventoryItem();
+    }, []);
 
     return (
         <section className="item">
@@ -19,30 +27,32 @@ function InventoryItemDetails() {
                     <p className="item__button-text"> Edit</p>
                 </button>
             </div>
-            <div className="item__description-div">
-                <h3 className="item__category-header">ITEM DESCRIPTION:</h3>
-                <p className="item__category-description"></p>
-                <h3 className="item__category-header">CATEGORY:</h3>
-                <p className="item__category-description"></p>
-            </div>
-            <div className="item__details-div">
-                <div className="item__status-quant-div">
-                    <div className="item__status-div">
-                        <h3 className="item__category-header">STATUS:</h3>
-                        <p className="item__category-description">Decription</p>
-                    </div>
-                    <div className="item__quant-div">
-                        <h3 className="item__category-header">QUANTITY:</h3>
-                        <p className="item__category-description">000</p>
-                    </div>
+            <div className="item__content-div">
+                <div className="item__description-div">
+                    <h3 className="item__category-header">ITEM DESCRIPTION:</h3>
+                    <p className="item__category-description">text</p>
+                    <h3 className="item__category-header">CATEGORY:</h3>
+                    <p className="item__category-description"></p>
                 </div>
-                <div className="item__warehouse-div">
-                    <h3 className="item__category-header">WAREHOUSE:</h3>
-                    <p className="item__category-description">Warehouse Name</p>
+                <div className="item__details-div">
+                    <div className="item__status-quant-div">
+                        <div className="item__status-div">
+                            <h3 className="item__category-header">STATUS:</h3>
+                            <p className="item__category-description">Decription</p>
+                        </div>
+                        <div className="item__quant-div">
+                            <h3 className="item__category-header">QUANTITY:</h3>
+                            <p className="item__category-description">000</p>
+                        </div>
+                    </div>
+                    <div className="item__warehouse-div">
+                        <h3 className="item__category-header">WAREHOUSE:</h3>
+                        <p className="item__category-description">Warehouse Name</p>
+                    </div>
                 </div>
             </div>
         </section>
     )
 }
 
-export default InventoryItemDetails
+export default InventoryItemDetails;
