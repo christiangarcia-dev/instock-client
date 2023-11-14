4e0e765e2a1f0ee4c7aeae273ac28f5adc89cd19
CryptoTotalWar <pablo@dltpartners.io>
2023-11-14 00:14:40 -0500

    add edit inventory page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 // import './App.scss';
-import AddNewWarehouse from './pages/AddNewWarehouse/AddNewWarehouse';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage';
-import HomePage from './pages/HomePage/HomePage';
-import InventoryListPage from './pages/InventoryListPage/InventoryListPage';
-import EditWarehouse from '../src/pages/EditWarehouse/EditWarehouse';
-import InventoryItemDetails from './pages/InventoryItemDetails/InventoryItemDetails';
+import AddNewWarehouse from "./pages/AddNewWarehouse/AddNewWarehouse";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage";
+import HomePage from "./pages/HomePage/HomePage";
+import InventoryListPage from "./pages/InventoryListPage/InventoryListPage";
+import EditWarehouse from "./pages/EditWarehouse/EditWarehouse";
+import EditInventory from "./pages/EditInventory/EditInventory";
+import InventoryItemDetails from "./pages/InventoryItemDetails/InventoryItemDetails";
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage />}/>
-          <Route path='/warehouse-details/:id' element={<WarehouseDetailsPage />} />
-          <Route path='/inventory'element={<InventoryListPage/>} />
-          <Route path= '/edit-warehouse-form/:id' element ={<EditWarehouse />} />
-          <Route path='/add-warehouse-form' element = {<AddNewWarehouse />} />
-          <Route path='/inventory-details/:id' element= {<InventoryItemDetails />} />
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/warehouse-details/:id"
+            element={<WarehouseDetailsPage />}
+          />
+          <Route path="/inventory" element={<InventoryListPage />} />
+          <Route path="/edit-warehouse-form/:id" element={<EditWarehouse />} />
+          <Route path="/add-warehouse-form" element={<AddNewWarehouse />} />
+          <Route
+            path="/inventory-details/:id"
+            element={<InventoryItemDetails />}
+          />
+          <Route path="/edit-inventory-form" element={<EditInventory />} />
         </Routes>
       </BrowserRouter>
     </div>
